a239474246a6078d6a6b305e2c3f9fa5ed6a6c12
Draon con Color <14227820+draoncc@users.noreply.github.com>
2020-01-13 18:51:12 +0100

    Add nginx

---
+++ install.d/16-nginx.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+SCRIPT_PATH="$(dirname "$(readlink -f "$BASH_SOURCE")")"
+SCRIPT_NAME="$(basename "$BASH_SOURCE")"
+source $SCRIPT_PATH/../util.sh
+
+if dpkg -s nginx &>/dev/null; then log_info "nginx is already installed"
+else
+  sudo apt -y install nginx
+fi
