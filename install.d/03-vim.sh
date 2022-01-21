4bfd18503e8f0e01812f56e558d650e9886dcbde
draoncc <14227820+draoncc@users.noreply.github.com>
2022-01-22 00:44:18 +0100

    Install nvim via ppa:neovim-ppa/stable instead

--- install.d/03-vim.sh
+++ install.d/03-vim.sh
@@ -8,11 +8,9 @@ error_if_not_installed curl
 
 if assert_is_installed nvim; then log_info "nvim is already installed"
 else
-  cd $(mktemp -d)
-  curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
-  chmod u+x nvim.appimage
-  sudo mv nvim.appimage /usr/bin/nvim.appimage
-  sudo ln -s /usr/bin/nvim.appimage /usr/bin/nvim
+  sudo add-apt-repository -y ppa:neovim-ppa/stable
+  sudo apt update
+  sudo apt install -y neovim
 fi
 
 sudo apt -y install python3-pip 
