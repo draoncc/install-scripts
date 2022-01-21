0b33a32c92723d6e5dfaddde060754d2becab0a0
draoncc <14227820+draoncc@users.noreply.github.com>
2022-01-22 00:45:10 +0100

    Install deno & tsserver, necessary for neovim's ddc with Typescript support

---
+++ install.d/06-deno.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+SCRIPT_PATH="$(dirname "$(readlink -f "$BASH_SOURCE")")"
+SCRIPT_NAME="$(basename "$BASH_SOURCE")"
+source $SCRIPT_PATH/../util.sh
+
+error_if_not_installed curl
+
+if assert_is_installed deno; then log_info "deno is already installed"
+else
+  curl -fsSL https://deno.land/install.sh | sh
+fi
---
+++ install.d/33-tsserver.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+SCRIPT_PATH="$(dirname "$(readlink -f "$BASH_SOURCE")")"
+SCRIPT_NAME="$(basename "$BASH_SOURCE")"
+
+error_if_not_installed yarn
+
+if assert_is_installed tsserver; then log_info "tsserver is already installed"
+else
+  sudo yarn global add typescript typescript-language-server
+fi
